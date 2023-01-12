533d7d30003efaac7cef50753ac1a7216c6ad41f
dukcode <duk9741@gmail.com>
2023-01-12 13:33:29 +0900

    init

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("dukcode.core.options")
+require("dukcode.core.keymaps")
+require("dukcode.core.colorscheme")
+require("dukcode.plugins-setup")
+require("dukcode.plugins.comment")
+require("dukcode.plugins.nvim-tree")
+require("dukcode.plugins.lualine")
+require("dukcode.plugins.telescope")
+require("dukcode.plugins.nvim-cmp")
+require("dukcode.plugins.autopairs")
+require("dukcode.plugins.treesitter")
+
